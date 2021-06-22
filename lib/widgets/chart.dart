82b65a063ed98c3626df5afe65a2d8a3eee1814f
sho <ikeguchi.sho@gmail.com>
2021-06-22 22:47:47 +0900

    Add: chart_bar.dart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -23,12 +23,8 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
-
-      print(DateFormat.E(weekDay));
-      print(totalSum);
-
       return {
-        'day': DateFormat.E(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
@@ -40,7 +36,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
